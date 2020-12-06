fbad80251c93b81149a7993e3688ef6f0cf61cdd
joker192 <73433769+joker192@users.noreply.github.com>
2020-12-06 22:41:51 +0330

    Create joooon.py

---
+++ joooon.py
@@ -0,0 +1,10 @@
+import os
+print("made by M.r joooon ")
+print("@jokerkinger")
+print(" جهت کیر شدن پارا کوبص")
+joker = input("Enter your kiri name : ")
+os.system("pkg install toilet -y ")
+os.system("clear")
+king = "toilet -f mono12 -F gay "
+os.system("clear")
+os.system("figlet " + king)
